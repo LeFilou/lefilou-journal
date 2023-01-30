5f3bfd2203e1d0729bd5f24e03d6b1d7558df51b
lefilou <fliousalim@gmail.com>
2023-01-30 20:42:53 +0100

    conf: init studio

---
+++ blog/pages/post/[slug].tsx
@@ -0,0 +1,36 @@
+import client from '../../client'
+
+const Post = (props) => {
+    const { title = 'Missing title', name = 'Missing name' } = props.post
+    return (
+        <article>
+            <h1>{title}</h1>
+            <span>By {name}</span>
+        </article>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+        `*[_type == "post" && defined(slug.current)][].slug.current`
+    )
+
+    return {
+        paths: paths.map((slug) => ({ params: { slug } })),
+        fallback: true,
+    }
+}
+
+export async function getStaticProps(context) {
+    const { slug = "" } = context.params
+    const post = await client.fetch(`*[_type == "post" && slug.current == $slug][0]{title, "name": author->name}`, { slug })
+    console.log(post);
+    
+    return {
+        props: {
+            post
+        }
+    }
+}
+
+export default Post
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '155p3ud3',
+    dataset: 'production'
+  }
+})
