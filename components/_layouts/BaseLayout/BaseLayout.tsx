02b8bbcc23822a7466355c9b041d24335ac80d29
lefilou <fliousalim@gmail.com>
2023-11-19 21:48:33 -0500

    style: make the sidebar responsive

--- components/_layouts/BaseLayout/BaseLayout.tsx
+++ components/_layouts/BaseLayout/BaseLayout.tsx
@@ -7,10 +7,15 @@ export interface BaseLayoutProps {
 
 const BaseLayout = ({ children }: BaseLayoutProps) => (
     <>
-        <Sidebar blogName="Lefilou" blogAuthor="Salim Fliou" description="Lefilou's personal journal"/>
-        <main tw="sm:ml-64">{children}</main>
+        <Sidebar
+            blogName="Lefilou"
+            blogAuthor="Salim Fliou"
+            description="Lefilou's personal journal"
+            githubPage="https://github.com/LeFilou"
+            linkedinPage="https://www.linkedin.com/in/salim-fliou/"
+        />
+        <main tw="lg:ml-96 md:ml-80 md:mt-0 mt-80 ml-0 ">{children}</main>
     </>
 );
 
 export default BaseLayout;
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import BaseLayout from '../components/_layouts/BaseLayout/BaseLayout';
-
 const IndexPage = () => (
-    <BaseLayout>
         <h1>Hello there</h1>
-    </BaseLayout>
 );
 
 export default IndexPage;
